8afb52971bf558c1d9c41c95113d98e6f7529852
grafika10 <grafika10@ucione.local>
2023-03-01 17:38:17 +0100

    react first setup

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+
+export function Logo(){
+    return (<figure>
+      <img src={logo} className="App-logo" alt="logo" />
+      <figcaption>Logo</figcaption>
+      </figure>
+    );
+  }
+  
